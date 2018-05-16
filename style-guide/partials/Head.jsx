86da1ef5fa522ac65ac9d60a7aaacb67271dc25c
Konrad Dzwinel <kdzwinel@gmail.com>
2018-05-16 16:17:12 +0200

    Update icons

--- style-guide/partials/Head.jsx
+++ style-guide/partials/Head.jsx
@@ -7,9 +7,9 @@ const Head = props =>
     <meta name="description" content="Brainly Style Guide - Sketch export file" />
 
     <link href="https://styleguide.brainly.com/134.2.1/style-guide.css" rel="stylesheet" />
-    <script src="https://styleguide.brainly.com/images/icons-d5df026a36.js" defer></script>
-    <script src="https://styleguide.brainly.com/images/subjects-icons-05e888a881.js" defer></script>
-    <script src="https://styleguide.brainly.com/images/subjects-mono-icons-d5e829baef.js" defer></script>
+    <script src="https://styleguide.brainly.com/images/icons-1b40deaa8d.js" defer></script>
+    <script src="https://styleguide.brainly.com/images/subjects-icons-0676dae371.js" defer></script>
+    <script src="https://styleguide.brainly.com/images/subjects-mono-icons-d9de3cd626.js" defer></script>
     <script src="https://styleguide.brainly.com/images/math-symbols-icons-2936684655.js" defer></script>
 
     <style>
