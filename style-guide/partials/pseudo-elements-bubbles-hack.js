eb397a49676dbf44cda871fd24c55759704795cf
patrycjaradaczynska <patrycja.radaczynska@brainly.com>
2019-09-26 13:34:18 +0200

    Update eslint config

--- style-guide/partials/pseudo-elements-bubbles-hack.js
+++ style-guide/partials/pseudo-elements-bubbles-hack.js
@@ -7,22 +7,28 @@ style.appendChild(document.createTextNode(css));
 head.appendChild(style);
 
 const allElements = document.querySelectorAll('.sg-bubble');
+/* eslint-disable max-len */
 const rect = '<svg width="24px" height="24px" viewBox="0 0 24 24" version="1.1" xmlns="http://www.w3.org/2000/svg"><rect id="Rectangle" fill="#FFFFFF" fill-rule="nonzero" transform="translate(17.000000, 17.000000) scale(-1, 1) rotate(45.000000) translate(-17.000000, -17.000000) " x="5" y="5" width="14" height="14"></rect></svg>';
+/* eslint-enable max-len */
 
 for (let i = 0; i < allElements.length; i++) {
-  
+
   const elementBeforeStyles = window.getComputedStyle(allElements[i], '::before');
   const values = {
     left: parseInt(elementBeforeStyles.left, 10) / 2,
     right: parseInt(elementBeforeStyles.right, 10) / 2,
     top: parseInt(elementBeforeStyles.top, 10) / 2,
     bottom: parseInt(elementBeforeStyles.bottom, 10) / 2,
-  }
+  };
+  /* eslint-disable max-len */
   const rectPositionStyles = `left: ${values.left}px; top: ${values.top}px; right: ${values.right}px; bottom: ${values.bottom}px; position: absolute`;
+  /* eslint-enable max-len */
+
   const elementAfterStyles = window.getComputedStyle(allElements[i], '::after');
-  
+
   if (elementBeforeStyles) {
     const virtualBefore = document.createElement('span');
+
     virtualBefore.style = `${rectPositionStyles}`;
     virtualBefore.innerHTML = rect;
     allElements[i].className += ' before-reset';
@@ -31,6 +37,7 @@ for (let i = 0; i < allElements.length; i++) {
 
   if (elementAfterStyles) {
     const virtualAfter = document.createElement('span');
+
     allElements[i].className += ' after-reset';
     allElements[i].appendChild(virtualAfter);
   }
