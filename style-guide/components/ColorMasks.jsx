8191239aa0d0758208e1907a21a021746756b16d
Mateusz Zimowski <mat.zimowski@gmail.com>
2020-04-29 08:59:26 +0200

    Bump style-guide to v161.4.0 and add 3 new color masks (#29)
    * Bump style-guide to v161.4.0 and add 3 new color masks
    
    * Up

--- style-guide/components/ColorMasks.jsx
+++ style-guide/components/ColorMasks.jsx
@@ -3,9 +3,30 @@ import colors from 'brainly-style-guide/src/components/colors/colors';
 
 const ColorMasksPage = () => {
   const colorMasksComponents = [];
+  const outOfStyleGuideColors = [
+    {
+      name: 'Gray Dark 1100',
+      hex: '1C2126'
+    },
+    {
+      name: 'Gray Dark 1000',
+      hex: '252C33'
+    },
+    {
+      name: 'Gray Dark 800',
+      hex: '46535F'
+    } 
+  ];
+  const enhancedColors = {
+    ...colors,
+    gray: [
+      ...colors.gray,
+      ...outOfStyleGuideColors,
+    ]
+  };
 
-  Object.keys(colors).forEach(category => {
-    colors[category].forEach(color => {
+  Object.keys(enhancedColors).forEach(category => {
+    enhancedColors[category].forEach(color => {
       const name = `ColorMask/${category}/${color.name}`;
 
       colorMasksComponents.push(
