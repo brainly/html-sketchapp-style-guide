317b0cc42220d1875b63328711eea95ec4df21c0
matzimowski <mat.zimowski@gmail.com>
2020-05-15 10:05:09 +0200

    Add toggle buttons

--- style-guide/components/Button.jsx
+++ style-guide/components/Button.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
-import Button, {BUTTON_TYPE, BUTTON_SIZE} from 'brainly-style-guide/src/components/buttons/Button';
+import Button, {
+  BUTTON_TYPE,
+  BUTTON_SIZE,
+} from 'brainly-style-guide/src/components/buttons/Button';
 import Icon from 'brainly-style-guide/src/components/icons/Icon';
 import {getValues} from '../utils/getValues';
 
@@ -8,42 +11,69 @@ const ButtonsPage = () => {
 
   getValues(BUTTON_TYPE, false).forEach(type => {
     getValues(BUTTON_SIZE, false).forEach(size => {
+      [null, 'peach', 'mustard', 'default'].forEach(toggle => {
+        [false, true].forEach(disabled => {
+          [false, true].forEach(icon => {
+            if (
+              (toggle &&
+                [
+                  'solid',
+                  'solid-inverted',
+                  'solid-blue',
+                  'solid-mint',
+                  'transparent-inverted',
+                  'facebook',
+                ].includes(type)) ||
+              (toggle === 'peach' && type === 'transparent-mustard') ||
+              (toggle === 'default' && type === 'transparent-mustard') ||
+              (toggle === 'mustard' && type === 'transparent-peach') ||
+              (toggle === 'default' && type === 'transparent-peach') ||
+              (toggle === 'default' && !icon)
+            ) {
+              return;
+            }
 
-      [false, true].forEach(disabled => {
-        [false, true].forEach(icon => {
-          let iconSize;
+            let iconSize;
 
-          if (size === 'large') {
-            iconSize = 32;
-          } else if (size === 'small') {
-            iconSize = 16;
-          } else {
-            iconSize = 24;
-          }
+            if (size === 'large') {
+              iconSize = 32;
+            } else if (size === 'small') {
+              iconSize = 16;
+            } else {
+              iconSize = 24;
+            }
 
-          const name =
-              `Button/${type}/${size}/${icon ? 'with icon' : '_default'}/ ${disabled ? 'disabled' : '_default'}`;
+            const name = `Button/${type}/${size}/${
+              icon ? 'with icon' : '_default'
+            }/${toggle ? `toggle-${toggle}` : '_default'}/${disabled ? 'disabled' : '_default'}`;
 
-          buttonsVariations.push(<div title={name} className='inline-item'>
-            <Button
-              icon={icon ? <Icon type="answer" color="peach" size={iconSize} /> : null}
-              type={type}
-              size={size}
-              disabled={disabled}
-              className="fix-button"
-            >Button</Button>
+            buttonsVariations.push(
+              <div title={name} className='inline-item'>
+                <Button
+                  icon={
+                    icon ? (
+                      <Icon type={toggle ? 'heart' : 'heart_outlined'} color='adaptive' size={iconSize} />
+                    ) : null
+                  }
+                  toggle={toggle}
+                  type={type}
+                  size={size}
+                  disabled={disabled}
+                  className='fix-button'
+                >
+                  Button
+                </Button>
+              </div>
+            );
+          });
 
-          </div>);
+          buttonsVariations.push(<br />);
         });
-
-        buttonsVariations.push(<br/>);
       });
     });
   });
 
-  return <section>
-    {buttonsVariations}
-  </section>;
+  return <section>{buttonsVariations}</section>;
 };
 
 export default ButtonsPage;
