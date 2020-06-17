773899b92e5884b6c8d8a47268694063ad497384
Mateusz Zimowski <mat.zimowski@gmail.com>
2020-06-17 06:41:31 +0200

    Bump style guide to 163.0.0 (#34)
    * Bump style-guide to 162.0.0
    
    * Up

--- style-guide/partials/Head.jsx
+++ style-guide/partials/Head.jsx
@@ -6,9 +6,9 @@ const Head = props =>
     <title>Brainly Style Guide - {props.version}</title>
     <meta name="description" content="Brainly Style Guide - Sketch export file" />
 
-    <link href="https://styleguide.brainly.com/161.8.2/style-guide.css" rel="stylesheet" />
-    <script src="https://styleguide.brainly.com/images/icons-0c5f18cafc.js" defer></script>
-    <script src="https://styleguide.brainly.com/images/mobile-icons-4571267a9d.js" defer></script>
+    <link href="https://styleguide.brainly.com/163.0.0/style-guide.css" rel="stylesheet" />
+    <script src="https://styleguide.brainly.com/images/icons-ab1f4d4a4b.js" defer></script>
+    <script src="https://styleguide.brainly.com/images/mobile-icons-48435e07e8.js" defer></script>
     <script src="https://styleguide.brainly.com/images/subjects-icons-a01adb2d40.js" defer></script>
     <script src="https://styleguide.brainly.com/images/subjects-mono-icons-341dc27120.js" defer></script>
     <script src="https://styleguide.brainly.com/images/math-symbols-icons-7c9d6883f2.js" defer></script>
