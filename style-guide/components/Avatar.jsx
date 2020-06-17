28b79af4ce0c75ff55f132ecd411d412720bcaad
matzimowski <mat.zimowski@gmail.com>
2020-06-17 09:10:44 +0200

    Up

--- style-guide/components/Avatar.jsx
+++ style-guide/components/Avatar.jsx
@@ -8,26 +8,9 @@ const AvatarsPage = () => {
   getValues(SIZE, false).forEach(size => {
     [false, true].forEach(border => {
       [null, 'https://source.unsplash.com/240x240/?cat'].forEach(src => {
-        let avatarSize;
-
-        if (size === 'xxs') {
-          avatarSize = '16';
-        } else if (size === 'xs') {
-          avatarSize = '24';
-        } else if (size === 's') {
-          avatarSize = '32';
-        } else if (size === 'm') {
-          avatarSize = '40';
-        } else if (size === 'l') {
-          avatarSize = '56';
-        } else if (size === 'xl') {
-          avatarSize = '80';
-        } else if (size === 'xxl') {
-          avatarSize = '104';
-        }
 
         const name =
-        `Avatar/${avatarSize}/${border ? 'border' : 'no-border'}/${src ? 'with image' : 'without image'}`;
+        `Avatar/${size}/${border ? 'border' : 'no-border'}/${src ? 'with image' : 'without image'}`;
 
         avatarVariations.push(<div key={name} title={name} className='inline-item'>
           <Avatar size={size} border={border} imgSrc={src} />
