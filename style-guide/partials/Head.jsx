6f659ed3a1f2e7ed3ec0e8e9640af24f18cd09e2
matzimowski <mat.zimowski@gmail.com>
2020-07-17 10:39:55 +0200

    Bumpy style-guide to v174.3.1

--- style-guide/partials/Head.jsx
+++ style-guide/partials/Head.jsx
@@ -6,8 +6,8 @@ const Head = props =>
     <title>Brainly Style Guide - {props.version}</title>
     <meta name="description" content="Brainly Style Guide - Sketch export file" />
 
-    <link href="https://styleguide.brainly.com/174.2.0/style-guide.css" rel="stylesheet" />
-    <script src="https://styleguide.brainly.com/images/icons-c6f28c9266.js" defer></script>
+    <link href="https://styleguide.brainly.com/174.3.1/style-guide.css" rel="stylesheet" />
+    <script src="https://styleguide.brainly.com/images/icons-b0350c5ec9.js" defer></script>
     <script src="https://styleguide.brainly.com/images/mobile-icons-48435e07e8.js" defer></script>
     <script src="https://styleguide.brainly.com/images/subjects-icons-a01adb2d40.js" defer></script>
     <script src="https://styleguide.brainly.com/images/subjects-mono-icons-341dc27120.js" defer></script>
