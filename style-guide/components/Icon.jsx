74703df85effe1988537458401920dfccb3d21f2
matzimowski <mat.zimowski@gmail.com>
2020-08-28 14:12:09 +0200

    Make eslint happy

--- style-guide/components/Icon.jsx
+++ style-guide/components/Icon.jsx
@@ -6,7 +6,7 @@ import Icon, {
 import MobileIcon, {
   TYPE as MOBILE_ICON_TYPE,
 } from 'brainly-style-guide/src/components/mobile-icons/MobileIcon';
-import { getValues } from '../utils/getValues';
+import {getValues} from '../utils/getValues';
 
 const LIMITED_SIZE = [16, 24, 32, 56, 80, 104]; // This is the final (or no) set of size for icons
 const IconsPage = () => {
@@ -15,11 +15,11 @@ const IconsPage = () => {
   const iconTypes = getValues(TYPE, false);
   const mobileIconTypes = getValues(MOBILE_ICON_TYPE, false);
   const uniqueMobileIconTypes = mobileIconTypes.filter(
-    (mobileIconType) => !iconTypes.includes(mobileIconType)
+    mobileIconType => !iconTypes.includes(mobileIconType)
   );
 
-  iconTypes.forEach((type) => {
-    getValues(LIMITED_SIZE, false).forEach((size) => {
+  iconTypes.forEach(type => {
+    getValues(LIMITED_SIZE, false).forEach(size => {
       const name = `Icon/${type}/${size}`;
 
       variations.push(
@@ -31,8 +31,8 @@ const IconsPage = () => {
     });
   });
 
-  uniqueMobileIconTypes.forEach((type) => {
-    getValues(LIMITED_SIZE, false).forEach((size) => {
+  uniqueMobileIconTypes.forEach(type => {
+    getValues(LIMITED_SIZE, false).forEach(size => {
       const name = `Icon/${type}/${size}`;
 
       variations.push(
