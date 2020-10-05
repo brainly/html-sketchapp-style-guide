4acea1eb91949f87ed3a64ada62c8dfc0dab1a92
Mike Skowronek <skowronkow@gmail.com>
2020-10-05 16:07:59 +0200

    Use proper icon sizes (#49)

--- style-guide/components/Icon.jsx
+++ style-guide/components/Icon.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 import Icon, {
   TYPE,
   ICON_COLOR,
+  SIZE,
 } from 'brainly-style-guide/src/components/icons/Icon';
 import MobileIcon, {
   TYPE as MOBILE_ICON_TYPE,
 } from 'brainly-style-guide/src/components/mobile-icons/MobileIcon';
 import {getValues} from '../utils/getValues';
 
-const LIMITED_SIZE = [16, 24, 32, 56, 80, 104]; // This is the final (or no) set of size for icons
 const IconsPage = () => {
   const variations = [];
 
@@ -19,7 +19,7 @@ const IconsPage = () => {
   );
 
   iconTypes.forEach(type => {
-    getValues(LIMITED_SIZE, false).forEach(size => {
+    getValues(SIZE, false).forEach(size => {
       const name = `Icon/${type}/${size}`;
 
       variations.push(
@@ -32,7 +32,7 @@ const IconsPage = () => {
   });
 
   uniqueMobileIconTypes.forEach(type => {
-    getValues(LIMITED_SIZE, false).forEach(size => {
+    getValues(SIZE, false).forEach(size => {
       const name = `Icon/${type}/${size}`;
 
       variations.push(
